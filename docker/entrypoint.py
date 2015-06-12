8f264ffbd589f53932593711e22f1ef11a3145f8
kendall libby <klibby@mozilla.com>
2015-06-12 09:13:14 -0400

    first pass at changing to use central config and call trees by name rather than url

--- docker/entrypoint.py
+++ docker/entrypoint.py
@@ -16,7 +16,9 @@ from lxml import etree
 
 dxr_home = os.environ['DXR_HOME']
 cfg_dir = dxr_home
-config = os.path.join(cfg_dir, 'entrypoint-config.yml')
+config_file = os.path.join(cfg_dir, 'config.yml')
+git_re = re.compile(r'git(hub\.com|\.mozilla\.(org|com))')
+hg_re = re.compile(r'hg\.mozilla\.org')
 
 
 class Unbuffered(object):
@@ -40,21 +42,11 @@ sys.stdout = Unbuffered(sys.stdout)
 sys.stderr = sys.stdout
 
 
-class SetupRepo(object):
+class IndexTree(object):
 
-    def __init__(self, url):
-        self.fullurl = url
-        self.url = urlparse.urlsplit(self.fullurl)
-        self.path = self.url.path.lstrip('/')
-        self.hg_or_git(url)
-        self.cfg_name = '{0}{1}'.format(self.url.netloc, self.url.path)
-        self.load_config(config, self.cfg_name)
-        if 'es_name' not in self.config:
-            self.config['es_name'] = self.path.replace("/", ":").lower()
-        self.config['source_dest'] = os.path.join('src', self.path)
-        self.config['source_parent'] = os.path.dirname(
-            self.config['source_dest'])
-        self.config['object_dir'] = os.path.join('obj', self.path)
+    def __init__(self, tree):
+        self.tree = tree
+        self.load_config(config_file, self.tree)
         self.template_env = Environment(
             loader=FileSystemLoader(os.path.join(cfg_dir, 'templates')),
             keep_trailing_newline=False,
@@ -62,85 +54,124 @@ class SetupRepo(object):
             trim_blocks=False,
         )
         self.templates = {
-            'dxr_config.j2': os.path.join(dxr_home, 'dxr.config'),
             'mozconfig.j2': os.path.join(self.config['source_dest'],
                                          '.mozconfig'),
         }
 
-    def load_config(self, config_file, repo_path):
-        f = open(config_file, 'r')
-        cfg = yaml.safe_load(f)
+    # if url were in dxr.config we could use that instead
+    def load_config(self, config, tree):
         try:
-            self.config = cfg[repo_path]
-        except KeyError:
-            self.config = cfg['default']
-        f.close()
+            f = open(config, 'r')
+            cfg = yaml.safe_load(f)
+            self.config = cfg[tree]
+        except IOError as e:
+            print "I/O error({0}): {1}".format(e.errno, e.strerror)
+        except (KeyError, AttributeError):
+            print "Missing entry for tree:", tree
+        else:
+            f.close()
+        if 'source_folder' not in self.config:
+            self.config['source_folder'] = os.path.join('src',
+                                                        self.tree)
+        url = urlparse.urlsplit(self.config['url'])
+        self.config['netloc'] = url.netloc
+        if git_re.match(url.netloc):
+            self.cmd = 'git'
+            self.update_cmd = ['fetch', '-u']
+        elif hg_re.match(url.netloc):
+            self.cmd = 'hg'
+            self.update_cmd = ['pull', '-u']
+        else:
+            # TODO: raise error
+            print 'No match'
 
     def render_template(self, template):
         return self.template_env.get_template(template).render(self.config)
 
-    def create_config_files(self):
-        for t in self.templates:
-            if t == 'mozconfig.j2' and not self.config['build_command']:
-                continue
-            elif t == 'mozconfig.j2' and os.path.exists(self.templates[t]):
-                continue
-            else:
-                with open(self.templates[t], 'a') as f:
-                    contents = self.render_template(t)
-                    f.write(contents)
-                    f.close()
-
-    def hg_or_git(self, url):
-        git_re = re.compile(r'git(hub\.com|\.mozilla\.(org|com))')
-        hg_re = re.compile(r'hg\.mozilla\.org')
-        if git_re.match(self.url.netloc):
-            self.cmd = 'git'
-            self.update_cmd = ['fetch', '-u']
-        elif hg_re.match(self.url.netloc):
-            self.cmd = 'hg'
-            self.update_cmd = ['pull', '-u']
+    def create_mozconfig(self):
+        mozconfig = os.path.join(self.config['source_folder'], '.mozconfig')
+        if not self.config['build_command']:
+            continue
+        elif os.path.exists(mozconfig):
+            continue
         else:
-            print 'No match'
+            with open(mozconfig, 'a') as f:
+                contents = self.render_template('mozconfig.j2')
+                f.write(contents)
+                f.close()
 
-    def clone_or_update(self):
-        if not os.path.exists(self.config['source_dest']):
+    def scrape_page(url, xpath):
+        page = requests.get(url)
+        return etree.HTML(page.content).xpath(xpath)
+
+    def scrape_hgmo(self, url):
+        repos = self.scrape_page(url, "//td/a[@class='list']/b/text()")
+        return [url + r for r in repos]
+
+    def scrape_gitmo(self, url):
+        u = urlparse.urlsplit(url)
+        baseurl = "{0}://{1}/".format(u.scheme, u.netloc)
+        repos = self.scrape_page(baseurl, "//td/a[@class='list']/text()")
+        regex = re.compile('^%s.*' % u.path[1:])
+        return [baseurl + r for r in repos if regex.search(r)]
+
+    def scrape_gh(url):
+        # must use api.github.com/{users,org}/name/
+        url += 'repos'
+        req_json = requests.get(url).json()
+        return [r["clone_url"] for r in req_json]
+
+    def scrape(self, url):
+        host = self.config['netloc']
+        if host == 'hg.mozilla.org':
+            repos = self.scrape_hgmo(url)
+        elif host == 'git.mozilla.org':
+            repos = self.scrape_gitmo(url)
+        elif host == 'github.com':
+            repos = self.scrape_gh(url)
+        else:
+            # TODO: raise error
+            print "error"
+        return repos
+
+    def update_src(self):
+        url = self.config['url']
+        source_dir = self.config['source_folder']
+        parent_dir = os.path.dirname(source_dir)
+        glob_re = re.compile('/\*$')
+        if not os.path.exists(source_dir):
             try:
-                os.stat(self.config['source_parent'])
+                os.lstat(parent_dir)
             except:
-                os.makedirs(self.config['source_parent'], 0755)
-            subprocess.check_call([self.cmd, 'clone', self.fullurl],
-                                  cwd=self.config['source_parent'])
+                os.makedirs(parent_dir, 0755)
+            if glob_re.search(url):
+                repos = self.scrape(url[:-1])
+            else:
+                repos = [url]
+            for r in repos:
+                subprocess.check_call([self.cmd, 'clone', r], cwd=parent_dir)
         else:
-            subprocess.check_call([self.cmd] + self.update_cmd,
-                                  cwd=self.config['source_dest'])
+            if glob_re.search(url):
+                dir_list = os.listdir(source_dir)
+                # TODO: clean this up
+                dirs = [os.path.join(source_dir, d) for d in dir_list if
+                        os.path.isdir(os.path.join(source_dir, d))]
+            else:
+                dirs = [source_dir]
+            for d in dirs:
+                subprocess.check_call([self.cmd] + self.update_cmd, cwd=d)
 
-
-def main(url):
-    repo = SetupRepo(url)
-    try:
-        repo.clone_or_update()
-        repo.create_config_files()
-    except subprocess.CalledProcessError as e:
-        print "Error: {0} returned {1}".format(e.cmd, e.output)
-    except:
-        print "Something went wrong with %s!", repo
 
 if __name__ == "__main__":
-    for url in sys.argv[1:]:
-        # test for proto://host/* and scrape
-        url_re = re.compile('/\*$')
-        if url_re.search(url):
-            baseurl = url[:-1]
-            page = requests.get(baseurl)
-            tree = etree.HTML(page.content)
-            # only works for hg.m.o!
-            repos = tree.xpath("//td/a[@class='list']/b/text()")
-            repos = [baseurl + r for r in repos]
-            for r in repos:
-                main(r)
-        else:
-            main(url)
+    for name in sys.argv[1:]:
+        project = IndexTree(name)
+        try:
+            project.update_src()
+        except subprocess.CalledProcessError as e:
+            print "Error: {0} returned {1}".format(e.cmd, e.output)
+        except:
+            print "Something went wrong with %s!", project
+        project.create_mozconfig()
         sys.stdout.flush()
         os.execv('venv/bin/dxr',
                  ['venv/bin/dxr', 'index', '-v', '--config', 'dxr.config'])
